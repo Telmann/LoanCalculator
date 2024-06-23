0f53128a732d0b07560df5188282ab3ca2d9f795
Владислав Тельман <John1111Wayne@yandex.ru>
2024-06-23 14:04:42 +0500

    fixed some PEP8 mistakes/errors

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import Tk, Label, Entry, Button
 
 
 class LoanCalculatorApp:
@@ -15,28 +15,34 @@ class LoanCalculatorApp:
         self.entry_interest_rate = Entry(window)  # not right column number
         self.entry_interest_rate.grid(column=1, row=0)
 
-        Label(window, background='#F1D7DA', text='Number Of Years: ', font=("Helvetica", 13)).grid(
-            column=0, row=1, padx=10, pady=10, sticky='w')
+        Label(window, background='#F1D7DA', text='Number Of Years: ',
+              font=("Helvetica", 13)).grid(
+              column=0, row=1, padx=10, pady=10, sticky='w')
 
         self.entry_number_years = Entry(window)
         self.entry_number_years.grid(column=1, row=1)
 
         Label(window, background='#F1D7DA', text='Loan amount: ',
-              font=("Helvetica", 13)).grid(column=0, row=2, padx=10, pady=10, sticky='w')
+              font=("Helvetica", 13)).grid(
+              column=0, row=2, padx=10, pady=10, sticky='w')
 
         self.entry_loan_amount = Entry(window)
         self.entry_loan_amount.grid(column=1, row=2)
 
         Label(window, background='#F1D7DA', text='Monthly Payment: ',
-              font=("Helvetica", 13)).grid(column=0, row=3, padx=10, pady=10)
+              font=("Helvetica", 13)).grid(
+              column=0, row=3, padx=10, pady=10)
 
         Label(window, background='#F1D7DA', text='Total Payment: ',
-              font=("Helvetica", 13)).grid(column=0, row=4, padx=10, pady=10)
+              font=("Helvetica", 13)).grid(
+              column=0, row=4, padx=10, pady=10)
 
         Button(window, text='Compute Payment', command=self.compute_payment,
-               font=("Helvetica", 12), bg="#4CAF50", fg="white").grid(column=1, row=5)
+               font=("Helvetica", 12), bg="#4CAF50", fg="white").grid(
+               column=1, row=5)
 
-        self.result_monthly_payment = Label(window, background='#F1D7DA', text='', font=("Helvetica", 14))
+        self.result_monthly_payment = Label(window, background='#F1D7DA',
+                                            text='', font=("Helvetica", 14))
         self.result_monthly_payment.grid(column=1, row=3,
                                          padx=10, pady=10)
 
@@ -47,10 +53,10 @@ class LoanCalculatorApp:
 
         window.mainloop()
 
-    def get_monthly_payment(self, loan_amount, monthly_interest_rate, number_of_years):  # надо исправить рассчет первый
+    def get_monthly_payment(self, loan_amount,
+                            monthly_interest_rate, number_of_years):
         monthly_payment = loan_amount * monthly_interest_rate / (1
-                                                                 - 1 / (1 + monthly_interest_rate) ** (
-                                                                         number_of_years * 12))
+        - 1 / (1 + monthly_interest_rate) ** (number_of_years * 12))
         return monthly_payment
 
     def compute_payment(self):  # try and except (ValueError)
@@ -62,8 +68,10 @@ class LoanCalculatorApp:
         self.result_monthly_payment.config(
             text=f'Monthly Payment: {monthly_payment:.3f}')
 
-        total_payment = monthly_payment * (int(self.entry_number_years.get()) * 12)
-        self.result_total_payment.config(text=f'Total Payment: {total_payment:.3f}')
+        total_payment = monthly_payment * \
+                        (int(self.entry_number_years.get()) * 12)
+        self.result_total_payment.config(
+            text=f'Total Payment: {total_payment:.3f}')
 
 
 simple_loan_calc = LoanCalculatorApp()
